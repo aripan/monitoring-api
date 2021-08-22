6e767cc0ea65a18edff515a8863399e243cf6cd8
aripan <aripan017@gmail.com>
2021-08-22 14:43:02 +0200

    Parsing Request

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
 
 // dependencies
 const http = require('http');
+const url = require('url');
+const { StringDecoder } = require('string_decoder');
 
 // app object - module scaffolding
 const app = {};
@@ -26,8 +28,30 @@ app.createServer = () => {
 
 // handle Request Response
 app.handleReqRes = (req, res) => {
-    // response handle
-    res.end('Hello Programmers');
+    // request handling
+    // get the url and parse it
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    const method = req.method.toLowerCase();
+    const queryStringObject = parsedUrl.query;
+    const headersObject = req.headers;
+
+    // decoding the raw data from req.body
+    const decoder = new StringDecoder('utf-8');
+    let realData = '';
+
+    req.on('data', (buffer) => {
+        realData += decoder.write(buffer);
+    });
+
+    req.on('end', () => {
+        realData += decoder.end();
+
+        console.log(realData);
+        // response handle
+        res.end('Hello Programmers');
+    });
 };
 
 // start the server
