fa5fc1980086febb2c11fcadbd07d7f9d1f6a6a1
aripan <aripan017@gmail.com>
2021-08-22 19:09:32 +0200

    Setup user route

---
+++ handlers/routeHandlers/userHandler.js
@@ -0,0 +1,36 @@
+/*
+ * Title: User Handler
+ * Description: Handler to handle user related routes
+ * Author: Md Asaduzzaman Ripan
+ * Date: 22/08/2021
+ */
+
+// dependencies
+const data = require('../../lib/data');
+const { hash } = require('../../helpers/utilities');
+const { parseJSON } = require('../../helpers/utilities');
+
+// module scaffolding
+const handler = {};
+
+// determining that which method has been passed from the user
+handler.userHandler = (requestProperties, callback) => {
+    const acceptedMethods = ['get', 'post', 'put', 'delete'];
+    if (acceptedMethods.indexOf(requestProperties.method) > -1) {
+        handler._users[requestProperties.method](requestProperties, callback);
+    } else {
+        callback(405);
+    }
+};
+
+handler._users = {};
+
+handler._users.post = (requestProperties, callback) => {};
+
+handler._users.get = (requestProperties, callback) => {};
+
+handler._users.put = (requestProperties, callback) => {};
+
+handler._users.delete = (requestProperties, callback) => {};
+
+module.exports = handler;
