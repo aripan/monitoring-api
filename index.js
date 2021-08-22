04dd30af23dd891f3adbda8c799e075e72216fc7
aripan <aripan017@gmail.com>
2021-08-22 13:43:03 +0200

    Create Server

---
+++ index.js
@@ -0,0 +1,34 @@
+/*
+ * Title: Uptime Monitoring Application
+ * Description: A RESTful API to monitor up or down time of user defined links
+ * Author: Md Asaduzzaman Ripan
+ * Date: 22/08/2021
+ */
+
+// dependencies
+const http = require('http');
+
+// app object - module scaffolding
+const app = {};
+
+// configuration
+app.config = {
+    port: 3000,
+};
+
+// create server
+app.createServer = () => {
+    const server = http.createServer(app.handleReqRes);
+    server.listen(app.config.port, () => {
+        console.log(`listening to port ${app.config.port}`);
+    });
+};
+
+// handle Request Response
+app.handleReqRes = (req, res) => {
+    // response handle
+    res.end('Hello Programmers');
+};
+
+// start the server
+app.createServer();
