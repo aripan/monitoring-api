5a2906e6e98eb00e7d6d4a59ef1c60ea40c42074
aripan <aripan017@gmail.com>
2021-08-24 11:18:42 +0200

    Setup boilerplate for Twilio application

---
+++ helpers/notifications.js
@@ -0,0 +1,18 @@
+/*
+ * Title: Notifications Library
+ * Description: Important functions to notify users
+ * Author: Md Asaduzzaman Ripan
+ * Date: 24/08/2021
+ */
+
+// dependencies
+const https = require('https');
+
+// module scaffolding
+const notifications = {};
+
+// send sms to user using twilio api
+notifications.sendTwilioSms = (phone, msg, callback) => {};
+
+// export the module
+module.exports = notifications;
